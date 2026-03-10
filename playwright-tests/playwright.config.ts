7fde4d6cb1d43f7ab4cffab2958de0103b2c8243
Catur Ageng <caturageng023@gmail.com>
2026-03-10 17:59:20 +0700

    Created main configutation for Playwright

---
+++ playwright-tests/playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  //Direktori tempat tes berada
+  testDir: "./tests",
+
+  //Jalankan tes dalam file secara paralel
+  fullyParallel: true,
+
+  //Gagalkan build jika ada tes.only yang tidak sengaja di commit
+  forbidOnly: !!process.env.CI,
+
+  //Retry otomatis saat di CI
+  retries: process.env.CI ? 2 : 0,
+
+  //Jumlah worker paralel
+  workers: process.env.CI ? 1 : undefined,
+
+  //Format laporan hasil tes
+  reporter: [["html", {open: "never" }], ["list"]],
+
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry", //Rekam trace saat retry
+    screenshot: "only-on-failure", //Ambil screenshot hanya saat tes gagal
+    video: "retain-on-failure", //Rekam video hanya saat tes gagal
+  },
+
+  projects: [
+    { name: "chromium",       use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox",        use: { ...devices["Desktop Firefox"] } },
+    { name: "Mobile Chrome",  use: { ...devices["Pixel 5"] } },
+    { name: "Mobile Safari",  use: { ...devices["iPhone 12"] } },
+  ],
+});
